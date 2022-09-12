4850d578dca1ce0879758c60817ec2d75c6d240c
bigSmitty <rhsmith@mines.edu>
2022-09-11 22:23:43 -0600

    Implemented first iteration of plus/minus button in Burner.java

--- src/Burner.java
+++ src/Burner.java
@@ -7,7 +7,7 @@ public class Burner {
 	//References Setting.java
 	private Setting mySetting;
 	
-	//Timer not set, it's always set by other methods when relevant
+	
 	private int timer;
 	public static final int TIME_DURATION = 2;
 	
@@ -21,6 +21,65 @@ public class Burner {
 		super();
 		this.myTemperature = Temperature.COLD;
 		this.mySetting = Setting.OFF;
+		//We *shouldn't* have to set timer, but this might help avoid errors in the future
+		this.timer = 0;
+	}
+	
+	//Cycle burner up one heat, if possible
+	public void plusButton() {
+		//Using a switch statement for the purposes of learning
+		switch (this.mySetting) {
+			//using the "this" keyword isn't necessary
+			//But I like the legibility of it
+			case OFF:
+				this.mySetting = Setting.LOW;
+				break;
+				
+			case LOW:
+				this.mySetting = Setting.MEDIUM;
+				break;
+				
+			case MEDIUM:
+				this.mySetting = Setting.HIGH;
+				break;
+			
+			//Already on high, can't increase setting more
+			case HIGH:
+				break;
+		}
+		
+		//Reset time to update burner heat
+		this.timer = TIME_DURATION;
+	}
+	
+	//Cycle burner down one heat, if possible
+	public void minusButton() {
+		//Reset time to update burner heat
+		this.timer = TIME_DURATION;
+		
+		//Using a switch statement for the purposes of learning
+		switch (this.mySetting) {
+		    
+			case OFF:
+				//Already on low, can't decrease setting more
+				break;
+						
+			case LOW:
+				this.mySetting = Setting.OFF;
+				break;
+						
+			case MEDIUM:
+				this.mySetting = Setting.LOW;
+				break;
+		
+			
+			case HIGH:
+				this.mySetting = Setting.MEDIUM;
+				break;
+		}
+				
 		
 	}
+	
+	
 }
